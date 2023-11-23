66fccc93b7c696ba6e762e2cba1fd127872b863a
nkronner <nkronner@gmail.com>
2023-11-23 14:19:16 -0500

    Life_in_weeks project completed and added to practice directory

---
+++ Practice Python Files/Life_in_weeks_exercise.py
@@ -0,0 +1,11 @@
+#Life In Weeks Exercise
+
+age = input("What is your age? ")
+age = int(age)
+
+total_weeks_in_life = (4000)
+weeks_per_year = (52)
+weeks_of_life_now = age * weeks_per_year
+weeks_left = round(total_weeks_in_life - weeks_of_life_now)
+print(f"You have {weeks_left} weeks left of your life... Geez lol")
+
